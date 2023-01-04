729c5d2fefe892a70174685deff196b13063f2c6
Ted Tschopp <ted@tschopp.org>
2023-01-04 14:25:16 -0800

    Gamma World Creatures Update

--- _data/Convert-Gamma-World.py
+++ _data/Convert-Gamma-World.py
@@ -1,4 +1,9 @@
+# Convert json to yaml
+
+
+import sys
 import json
+from ruamel.yaml import YAML
 
 in_file_path='/Users/tedtschopp/Developer/tschopp.net/_data/GammaWorldv5 Working.json' # Change me!
 
@@ -7,14 +12,13 @@ with open(in_file_path,'r') as in_json_file:
     # Read the file and convert it to a dictionary
     json_obj_list = json.load(in_json_file)
 
-    for json_obj in json_obj_list:
+    for json_obj in json_obj_list['CreaturesV5']:
         i = 1
-        for creature in json_obj:
-           
-           filename='creature_'+str(i)+'.json'
-
-           with open(filename, 'w') as out_json_file:
-               # Save each obj to their respective filepath
-               # with pretty formatting thanks to `indent=4`
-               json.dump(creature, out_json_file, indent=4)
-               i=i+1
+        print (json_obj['Name']+' '+json_obj['Common_Name'])
+        
+        filename='creature_'+json_obj['Name']+'_'+json_obj['Common_Name']+'.yaml'
+        # print (i)
+        with open(filename, 'w') as out_yaml_file:
+            yaml = YAML()
+            yaml.default_flow_style = False
+            yaml.dump(json_obj, out_yaml_file)
