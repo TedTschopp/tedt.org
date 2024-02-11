35c7414445495df8b2a86e2d818f59ca42b058c7
Ted Tschopp <ted@tschopp.org>
2024-02-11 15:56:31 -0800

    fixes

--- js/add-back-links-to-headers.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var htmlContent = '<a href="javascript:history.back()" class="text-decoration-none float-end">&#x2191;</a>';
-
-    // Select all headline tags (h1 to h6)
-    $(':header').each(function() {
-        // Insert the HTML content as the first child of each headline tag
-        $(this).prepend(htmlContent);
-    });
-});
