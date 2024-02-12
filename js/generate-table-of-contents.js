d580654dd55c233174102c6676c4e6fd3ff9caf4
Ted Tschopp <ted@tschopp.org>
2024-02-12 06:52:44 -0800

    Refactor table of contents generation code

--- js/generate-table-of-contents.js
+++ js/generate-table-of-contents.js
@@ -1,33 +1,35 @@
 $(document).ready(function() {
     var toc = "";
     var level = 0;
-    var maxLevel = 3;
 
-    var mainContent = $("#main_content");
-    var content = mainContent.html();
+    document.getElementById("contents").innerHTML =
+        document.getElementById("contents").innerHTML.replace(
+            /<h([\d])>([^<]+)<\/h([\d])>/gi,
+            function (str, openLevel, titleText, closeLevel) {
+                if (openLevel != closeLevel) {
+                    return str;
+                }
 
-    content = content.replace(/<h([\d])>([^<]+)<\/h([\d])>/gi, function (str, openLevel, titleText, closeLevel) {
-        if (openLevel != closeLevel) {
-            console.log(openLevel);
-            return str + ' - ' + openLevel;
-        }
-        if (openLevel > level) {
-            toc += (new Array(openLevel - level + 1)).join("<ol>");
-        } else if (openLevel < level) {
-            toc += (new Array(level - openLevel + 1)).join("</ol>");
-        }
-        level = parseInt(openLevel);
+                if (openLevel > level) {
+                    toc += (new Array(openLevel - level + 1)).join("<ul>");
+                } else if (openLevel < level) {
+                    toc += (new Array(level - openLevel + 1)).join("</ul>");
+                }
 
-        var anchor = titleText.replace(/ /g, "_");
-        toc += "<li><a href=\"#" + anchor + "\">" + titleText + "</a></li>";
+                level = parseInt(openLevel);
 
-        return "<h" + openLevel + "><a name=\"" + anchor + "\">" + titleText + "</a></h" + closeLevel + ">";
-    });
+                var anchor = titleText.replace(/ /g, "_");
+                toc += "<li><a href=\"#" + anchor + "\">" + titleText
+                    + "</a></li>";
+
+                return "<h" + openLevel + "><a name=\"" + anchor + "\">"
+                    + titleText + "</a></h" + closeLevel + ">";
+            }
+        );
 
     if (level) {
-        toc += (new Array(level + 1)).join("</ol>");
+        toc += (new Array(level + 1)).join("</ul>");
     }
 
-    mainContent.html(content);
-    $("#toc").append(toc);
+    document.getElementById("toc").innerHTML += toc;
 });
