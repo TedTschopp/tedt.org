5091037dbe9503373c78bc6c04aa63de1588b0db
Ted Tschopp <ted@tschopp.org>
2024-02-11 20:23:00 -0800

    Fix JavaScript file names and remove unnecessary code

--- js/winerlinks-bottom.js
+++ js/winerlinks-bottom.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-  var existingReadyFunction = $.fn.ready;
-
-  $.fn.ready = function(fn) {
-    if (existingReadyFunction) {
-      existingReadyFunction();
-    }
-
-    var main_content = $('#main_content');
-    main_content.html(addAnchorTagsToParagraphs(main_content.html()));
-
-    if (fn) {
-      fn();
-    }
-  };
+  var main_content = $('#main_content');
+  main_content.html(addAnchorTagsToParagraphs(main_content.html()));
 });
