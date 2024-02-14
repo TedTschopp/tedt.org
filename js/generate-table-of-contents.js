aa2f58eaec7babac9f187c3e19254495f5dad8e0
Ted Tschopp <ted@tschopp.org>
2024-02-14 07:35:56 -0800

    Update generate-table-of-contents.js

--- js/generate-table-of-contents.js
+++ js/generate-table-of-contents.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
 }); 
 
 function htmlTableOfContents( documentRef ) {
+
     var documentRef = documentRef || document;
     var toc = documentRef.getElementById("toc");
-//  Use headings inside <article> only:
-  var headings = [].slice.call(documentRef.body.querySelectorAll('article h1, article h2, article h3, article h4, article h5, article h6'));
-//  var headings = [].slice.call(documentRef.body.querySelectorAll('h1, h2, h3, h4, h5, h6'));
+    //  Use headings inside <article> only:
+    var headings = [].slice.call(documentRef.body.querySelectorAll('article h1, article h2, article h3, article h4, article h5, article h6'));
+    //  var headings = [].slice.call(documentRef.body.querySelectorAll('h1, h2, h3, h4, h5, h6'));
+    var lastLevel = 0;
     headings.forEach(function (heading, index) {
         var ref = "toc" + index;
 
@@ -21,9 +23,40 @@ function htmlTableOfContents( documentRef ) {
         link.setAttribute( "href", "#"+ ref );
         link.textContent = heading.textContent;
 
-        var div = documentRef.createElement( "div" );
-        div.setAttribute( "class", heading.tagName.toLowerCase() );
-        div.appendChild( link );
-        toc.appendChild( div );
+        var unorderedList = documentRef.createElement( "ul" );
+
+        var listElement = documentRef.createElement( "li" );
+        listElement.appendChild( link );
+
+        var className = heading.tagName.toLowerCase();
+        var thisLevel = parseInt( className.charAt(1) );
+
+
+        // If the current heading is a higher level than the last one, create a new list
+        if ( thisLevel > lastLevel ) {
+            // open up a new list element and append it to the last item
+            unorderedList.appendChild( listElement );
+        } else if ( thisLevel < lastLevel ) {
+            // close off the last list and start a new one
+            var diff = lastLevel - thisLevel;
+            var parent = toc;
+            for ( var i = 0; i < diff; i++ ) {
+                parent = parent.parentNode;
+            }
+            parent.appendChild( listElement );
+        } else {
+            // add the list item to the parent
+            unorderedList.appendChild( listElement );
+        }
+
+        // var div = documentRef.createElement( "div" );
+        // div.setAttribute( "class", className );
+        // div.appendChild( link );
+        // toc.appendChild( div );
+
+        // set the last level to this level
+        lastLevel = thisLevel;
+
     });
+
 }
