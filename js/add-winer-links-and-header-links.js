fb7545825e52074c4b6848941e86406598d1900c
Ted Tschopp <ted@tschopp.org>
2024-06-04 10:56:02 -0700

    Refactor default.html and category.html to add lazy loading for images and generate table of contents

--- js/add-winer-links-and-header-links.js
+++ js/add-winer-links-and-header-links.js
@@ -12,6 +12,8 @@ $(document).ready(function() {
     var main_content = document.getElementById('main_content');
     main_content.innerHTML = addAnchorTagsToParagraphs(main_content.innerHTML);
 
+    GenerateTableOfContents();
+
 });
 
 function addAnchorTagsToParagraphs(html) {
@@ -71,3 +73,41 @@ function concatenateFirstLetters(text) {
 }
 
 
+function GenerateTableOfContents() {  
+    const toc = document.getElementById('insert-table-of-contents-here');
+    const headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    let tocItems = ['<ol>']; // Start with an opening <ol> tag
+    let currentLevel = 1;
+  
+    headers.forEach(header => {
+      const level = parseInt(header.tagName.substring(1), 10); // Get numeric part of heading tag (h1, h2, etc.)
+  
+      // Adjust the level depth
+      while (level > currentLevel) {
+        tocItems.push('<ol>');
+        currentLevel++;
+      }
+      while (level < currentLevel) {
+        tocItems.push('</li></ol>'); // Close current list and step out
+        currentLevel--;
+      }
+  
+      // Create ID and link for the header
+      const headerText = header.textContent;
+      const headerId = headerText.replace(/\s+/g, '-').toLowerCase().replace(/[^a-z0-9-]/gi, '');
+      header.id = headerId; // Assign ID to header
+  
+      tocItems.push(`<li><a href="#${headerId}">${headerText}</a>`); // Append the link wrapped in <li>
+    });
+  
+    // Close all open lists and items
+    while (currentLevel > 1) {
+      tocItems.push('</li></ol>'); // Properly close each level
+      currentLevel--;
+    }
+    tocItems.push('</li></ol>'); // Close the initial <ol>
+  
+    toc.innerHTML = tocItems.join(''); // Convert array to string and set as HTML once
+    toc.setAttribute('role', 'navigation');
+    toc.setAttribute('aria-label', 'Table of contents');
+  }
