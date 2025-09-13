691f6163b8854ba370f66f0cd877bcdccbcce45d
Ted Tschopp <ted@tschopp.org>
2025-09-13 16:19:15 -0700

    refactor: update recent posts pruning logic and re-enable full post set option

--- _plugins/ci_recent_posts.rb
+++ _plugins/ci_recent_posts.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/env ruby
-# Filters site.posts in CI to only include most recent N days (default 120) unless CI_DEBUG_FULL=1.
+# Recent posts pruning plugin (currently disabled).
+# Previously trimmed posts older than CI_RECENT_DAYS (default 120) to reduce memory.
+# User requested full post set restored in CI. To re-enable, set CI_RECENT_ENABLE=1.
+return unless ENV['CI_RECENT_ENABLE'] == '1'
 return if ENV['CI_DEBUG_FULL'] == '1'
 days = (ENV['CI_RECENT_DAYS'] || '120').to_i
 cutoff = Time.now - (days * 86400)
 Jekyll::Hooks.register :site, :post_read do |site|
   orig_count = site.posts.docs.size
   site.posts.docs.select! do |d|
-    t = d.data['date'] || d.date
+    t = d.data['update'] || d.data['date'] || d.date
     (t.is_a?(Time) ? t : Time.parse(t.to_s)) >= cutoff rescue true
   end
   filtered = site.posts.docs.size
